cc15c98a1815dec2e9388a8cf0466226f59a3ad7
FGC1 <616101621@qq.com>
2018-11-08 22:22:14 +0800

    convert.cpp

---
+++ chapter02/06-convert.cpp
@@ -0,0 +1,21 @@
+// convert.cpp -- converts stone to pounds
+#include <iostream>
+
+using namespace std;
+
+int stonetolb(int);	// function prototype
+
+int main()
+{
+	int stone;
+	cout << "Enter the weight in stone: ";
+	cin >> stone;
+	int pounds = stonetolb(stone);
+	cout << stone << " stone = " << pounds << " pounds." << endl;
+	return 0;
+}
+
+int stonetolb(int sts)
+{
+	return 14 * sts;
+}
