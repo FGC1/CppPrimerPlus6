cdd5aebc7de8e8d7eadee7132965d77ae099c205
FGC1 <616101621@qq.com>
2018-11-08 21:41:29 +0800

    myfirst.cpp

---
+++ chapter02/01-myfirst.cpp
@@ -0,0 +1,14 @@
+// myfirst.cpp -- displays a message
+
+#include <iostream>					// a preprocessor directive
+
+using namespace std;					// make difinitions visible
+
+int main()						// function header
+{							// start of function body
+	cout << "Come up and C++ me some time.";	// message
+	cout << endl;					// start a new line
+	cout << "You won't regret it!" << endl;		// more output
+
+	return 0;					// terminate main()
+}							// end of function body
