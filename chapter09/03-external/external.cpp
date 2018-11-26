11aad7a1eb3234370bd445c4f09ade1c28caaab7
FGC1 <616101621@qq.com>
2018-11-26 10:46:10 +0800

    添加了extern源码

---
+++ chapter09/03-external/external.cpp
@@ -0,0 +1,18 @@
+// external.cpp -- external variables
+// compile with support.cpp
+#include <iostream>
+
+double warming = 0.3;
+
+void update(double dt);
+void local();
+
+int main()
+{
+	std::cout << "Global warming is " << warming << " degrees.\n";
+	update(0.1);
+	std::cout << "Global warming is " << warming << " degrees.\n";
+	local();
+	std::cout << "Global warming is " << warming << " degrees.\n";
+	return 0;
+}
---
+++ chapter09/03-external/support.cpp
@@ -0,0 +1,23 @@
+// support.cpp -- use external variable
+// compile with external.cpp
+#include <iostream>
+extern double warming;
+
+void update(double dt);
+void local();
+
+void update(double dt)
+{
+	extern double warming;
+	warming += dt;
+	std::cout << "Updating global warming to " << warming;
+	std::cout << " degrees.\n";
+}
+
+void local()
+{
+	double warming = 0.8;
+	std::cout << "Local warming = " << warming << " degrees.\n";
+	std::cout << "But global warming = " << ::warming;
+	std::cout << " degrees.\n";
+}
