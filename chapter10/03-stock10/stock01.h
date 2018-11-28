7b0d5c0fc60e936af9bef6fad9660f6cec5c33bd
FGC1 <616101621@qq.com>
2018-11-28 13:09:48 +0800

    添加了stock的构造和析构函数

---
+++ chapter10/03-stock10/stock01.h
@@ -0,0 +1,26 @@
+// stock01.h -- Stock class interface
+// version 01
+#ifndef STOCK01_H_
+#define STOCK01_H_
+
+#include <string>
+
+class Stock
+{
+private:
+	std::string company;
+	long shares;
+	double share_val;
+	double total_val;
+	void set_tot() { total_val = shares * share_val; }
+
+public:
+	Stock();
+	Stock(const std::string &co, long n = 0, double pr = 0.0);
+	~Stock();
+	void buy(long num, double price);
+	void sell(long num, double price);
+	void update(double price);
+	void show();
+};
+#endif // STOCK01_H_
