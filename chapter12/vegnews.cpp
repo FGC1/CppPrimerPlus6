71d76011515ae661e81c94fed9b9550add7cd99c
FanGuichuan <616101621@qq.com>
2018-12-14 23:08:25 +0800

    使用StringBad类

---
+++ chapter12/vegnews.cpp
@@ -0,0 +1,52 @@
+// vegnews.cpp -- using new and delete with classes
+// compile with stringbad.cpp
+#include <iostream>
+#include "stringbad.h"
+
+void callme1(StringBad &);	// pass by reference
+void callme2(StringBad);	// pass by value
+
+int main()
+{
+	{
+		std::cout << "Starting an inner block.\n";
+		StringBad headline1("Celery Stalks at Midnight");
+		StringBad headline2("Lettuce Prey");
+		StringBad sports("Spinach Leaves Bowl for Dollars");
+
+		std::cout << "headline1: " << headline1 << std::endl;
+		std::cout << "headline2: " << headline2 << std::endl;
+		std::cout << "sports: " << sports << std::endl;
+
+		callme1(headline1);
+
+		std::cout << "headline1: " << headline1 << std::endl;
+
+		callme2(headline2);
+
+		std::cout << "headline2: " << headline2 << std::endl;
+		std::cout << "Initialize one object to another:\n";
+		StringBad sailor = sports;
+		std::cout << "sailor: " << sailor << std::endl;
+		std::cout << "Assign one object to another:\n";
+		StringBad knot;
+		knot = headline1;
+
+		std::cout << "knot: " << knot << std::endl;
+		std::cout << "Exiting the block.\n";
+	}
+	std::cout << "End of main()\n";
+	return 0;
+}
+
+void callme1(StringBad &rsb)
+{
+	std::cout << "String passed by reference:\n";
+	std::cout << "\t\"" << rsb << "\"\n";
+}
+
+void callme2(StringBad sb)
+{
+	std::cout << "String passed by value:\n";
+	std::cout << "\t\"" << sb << "\"\n";
+}
